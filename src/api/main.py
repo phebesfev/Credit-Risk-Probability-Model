a97458cec7f6acd67c9e7d4fdb7e93ce0b393223
Feven Alemayehu <fevenhaile231@gmail.com>
2025-07-03 23:42:14 +0300

    Implement model deployment API with FastAPI, containerize service with Docker

--- src/api/main.py
+++ src/api/main.py
@@ -1,1 +1,32 @@
 # FastAPI application
+import mlflow.sklearn
+import pandas as pd
+from fastapi import FastAPI, Request
+from src.api.pydantic_models import PredictionRequest,PredictionResponse
+
+app = FastAPI()
+
+model_name = "CreditRiskRandomForest"
+model_version = '1'
+model_uri = "src/mlruns/0/models/m-9e17dfac59bf4352aab1acce8d964513/artifacts"
+model = mlflow.sklearn.load_model(model_uri)
+
+
+
+@app.get("/")
+def root():
+    return {"message": "Credit Risk API is alive 🚀"}
+
+
+
+
+# === /predict endpoint ===
+@app.post("/predict",response_model=PredictionResponse)
+def predict(input_data: PredictionRequest):
+    # Convert Pydantic model to DataFrame
+    input_df = pd.DataFrame([input_data.dict()])
+
+    # Get fraud probability (class 1)
+    risk_proba = model.predict_proba(input_df)[0][1]
+
+    return {"risk_probability": risk_proba}
--- src/api/pydantic_models.py
+++ src/api/pydantic_models.py
@@ -1,1 +1,33 @@
  # Pydantic models for API
+from pydantic import BaseModel
+
+class PredictionRequest(BaseModel):
+    CountryCode: int
+    ProviderId: int
+    ProductId: int
+    ChannelId: int
+    Amount: float
+    Value: float
+    TransactionHour: int
+    TransactionDay: int
+    TransactionMonth: int
+    TransactionYear: int
+    PricingStrategy: int
+    ProductCategory_data_bundles: int
+    ProductCategory_financial_services: int
+    ProductCategory_movies: int
+    ProductCategory_other: int
+    ProductCategory_ticket: int
+    ProductCategory_transport: int
+    ProductCategory_tv: int
+    ProductCategory_utility_bill: int
+    total_transaction_amount: float
+    avg_transaction_amount: float
+    transaction_count: float
+    std_transaction_amount: float
+    is_high_risk: int
+
+class PredictionResponse(BaseModel):
+    risk_probability: float
+
+ 
